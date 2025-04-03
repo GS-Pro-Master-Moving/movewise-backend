48b9cd193718e7c801fdcc2da34da95234b231a2
StivenColorado <stivenchoo@gmail.com>
2025-04-02 19:49:39 -0500

    delete comment

--- api/person/serializers/PersonSerializer.py
+++ api/person/serializers/PersonSerializer.py
@@ -1,12 +1,3 @@
-# from rest_framework import serializers
-# from api.person.models.Person import Person
-
-# def PersonSerializer(person: Person) -> dict:
-#     class meta(serializers.ModelSerializer.Meta):
-#         model = Person
-#         # fields = "__all__"
-#         fields = ["email", "name", "last_name", "birth_date", "cell_phone", "address", "id_number", "type_id"]
-
 from rest_framework import serializers
 from api.models import Person
 
