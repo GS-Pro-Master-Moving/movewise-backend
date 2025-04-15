9bfb41ab54463f6aa184682cf7c18036952b6cc5
Juliandross <138513671+Juliandross2@users.noreply.github.com>
2025-04-15 09:04:39 -0500

    pagination_added

--- api/tool/controllers/ControllerTool.py
+++ api/tool/controllers/ControllerTool.py
@@ -1,5 +1,6 @@
-from rest_framework import status, viewsets
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
+from rest_framework import status, viewsets
 from api.tool.services.ServicesTool import ServicesTool
 from api.tool.serializers.SerializerTool import ToolSerializer
 
@@ -7,9 +8,11 @@ class ControllerTool(viewsets.ViewSet):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.tool_service = ServicesTool() 
+        self.tool_service = ServicesTool()
+        self.paginator = PageNumberPagination()  # Add a paginator instance
 
     def list(self, request):
         """Obtiene todas las herramientas"""
-        tools = self.tool_service.get_all_tools()  
-        return Response(ToolSerializer(tools, many=True).data)  
+        tools = self.tool_service.get_all_tools()
+        paginated_tools = self.paginator.paginate_queryset(tools, request)  # Paginate the queryset
+        return self.paginator.get_paginated_response(ToolSerializer(paginated_tools, many=True).data)
