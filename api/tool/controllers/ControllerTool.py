e1e59815d6e425a580d4ec7405045d8240a4215a
Juliandross <138513671+Juliandross2@users.noreply.github.com>
2025-05-14 23:18:34 -0500

    ToFix

--- api/tool/controllers/ControllerTool.py
+++ api/tool/controllers/ControllerTool.py
@@ -1,6 +1,7 @@
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework import status, viewsets
+from api.tool.models.Tool import Tool
 from api.tool.services.ServicesTool import ServicesTool
 from api.tool.serializers.SerializerTool import ToolSerializer
 
@@ -8,7 +9,6 @@ class ControllerTool(viewsets.ViewSet):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.tool_service = ServicesTool()
         self.paginator = PageNumberPagination()  # Add a paginator instance
 
     def list(self, request):
@@ -27,3 +27,64 @@ class ControllerTool(viewsets.ViewSet):
         response = self.paginator.get_paginated_response(serialized_data)
         response.data['current_company_id'] = company_id
         return response
+    def retrieve(self, request, pk=None):
+        """
+        Get a tool by ID
+        """
+        company_id = getattr(request, 'company_id', None)
+        if not company_id:
+            return Response(
+                {"detail": "The company ID was not found in the request"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        # Buscar la herramienta por ID y compañía
+        tool = Tool.objects.filter(id=pk, company_id=company_id).first()
+        if not tool:
+            return Response(
+                {"detail": "Tool not found or does not belong to the company"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        # Serializar y devolver la herramienta
+        serialized_data = ToolSerializer(tool).data
+        return Response(serialized_data, status=status.HTTP_200_OK)
+    
+    def create(self, request):
+        "Create a new tool"
+        company_id = getattr(request, 'company_id', None)
+        if not company_id:
+            return Response(
+                {"detail": "The company ID was not found in the request"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        serializer = ToolSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk=None):
+        "Set status of a tool to inactive"
+        company_id = getattr(request, 'company_id', None)
+        if not company_id:
+            return Response(
+                {"detail": "The company ID was not found in the request"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        tool = Tool.objects.filter(id=pk, company_id=company_id).first()
+        if not tool:
+            return Response(
+                {"detail": "Tool not found"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        tool.state = False
+        tool.save()
+        return Response(
+            {"detail": "Tool deleted successfully"},
+            status=status.HTTP_200_OK
+        )
+    
+    
+    
