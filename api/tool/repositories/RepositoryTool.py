82295c92b2eca9e5a5f39ceaddbc05cafc182672
Juliandross <138513671+Juliandross2@users.noreply.github.com>
2025-05-17 14:02:45 -0500

    ToolsByJob

--- api/tool/repositories/RepositoryTool.py
+++ api/tool/repositories/RepositoryTool.py
@@ -3,3 +3,9 @@ from api.tool.models.Tool import Tool
 class RepositoryTool:
     def get_all_tools(self):
         return Tool.objects.all()  
+
+    def get_job_tools(self, company_id, id_job):
+        return Tool.objects.filter(
+            company_id=company_id,
+            job_id=id_job
+        ).all()
